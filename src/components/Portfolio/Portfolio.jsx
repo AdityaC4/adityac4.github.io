fdb29dbf37219c8752eb9f63f39ba38310ce9a70
AdityaC4 <adityapchaudhari@gmail.com>
2024-07-16 00:05:10 -0400

    static test

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,15 @@
+const Portfolio = () => {
+  return (
+    <div id="portfolio" className="section">
+      <h1>Portfolio Section</h1>
+      <p>
+        This is the experience section. Here you can list your professional
+        experience, including your job title, company name, and dates of
+        employment. You can also include a brief description of your
+        responsibilities and accomplishments.
+      </p>
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Contact from "./Contact/Contact";
+import Experience from "./Experience/Experience";
+import Hero from "./Hero/Hero";
+import Portfolio from "./Portfolio/Portfolio";
+
+export { Contact, Experience, Hero, Portfolio };
