e2c6f77817dd58dbaed4e24849c9bf2212b46ac4
AdityaC4 <adityapchaudhari@gmail.com>
2024-07-17 20:55:52 -0400

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,22 @@
 import "./Hero.css";
+import gif1 from "../../assets/gifs/8ball.gif";
+import gif2 from "../../assets/gifs/glasses.gif";
 
 const Hero = () => {
   return (
     <div id="hero" className="section__hero section">
-      <div className="hero__name">Aditya</div>
-      <div className="hero__name">Chaudhari</div>
+      <div className="hero__overlay">
+        {/* Add your gifs and images here */}
+        <img src={gif2} alt="Overlay 2" className="hero__image img2" />
+      </div>
+      <div className="hero__content">
+        <h1 className="hero__name hero__first-name">Aditya</h1>
+        <h1 className="hero__name hero__last-name">Chaudhari</h1>
+      </div>
+      <div className="hero__overlay">
+        {/* Add your gifs and images here */}
+        <img src={gif1} alt="Overlay 1" className="hero__image img1" />
+      </div>
     </div>
   );
 };
